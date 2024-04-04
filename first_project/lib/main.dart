b229a6039114789fa741bee9e042ffcf3729e828
Bilal Ahmad <bilal.codemaster@gmail.com>
2024-04-04 08:13:06 -0700

    fee voucher project front end

--- first_project/lib/main.dart
+++ first_project/lib/main.dart
@@ -1,125 +1,387 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Fee Voucher App',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: HomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class HomePage extends StatelessWidget {
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            FeeVoucherButton(
+              label: 'Semester Fee Voucher',
+               onPressed: () {
+            // Navigate to the new window (or screen)
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => SemesterFeePage()),
+            );
+          },
+            ),
+            SizedBox(height: 20),
+            FeeVoucherButton(
+              label: 'Admission Fee Voucher',
+              onPressed: () {
+            // Navigate to the new window (or screen)
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => AdmissionFeePage()),
+            );
+          },
+            ),
+            SizedBox(height: 20),
+            FeeVoucherButton(
+              label: 'Repeat Course Fee Voucher',
+              onPressed: () {
+            // Navigate to the new window (or screen)
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => RepeatFeePage()),
+            );
+          },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
 
-  void _incrementCounter() {
+class SemesterFeePage extends StatefulWidget {
+  @override
+  _SemesterFeePageState createState() => _SemesterFeePageState();
+}
+
+class _SemesterFeePageState extends State<SemesterFeePage> {
+  final TextEditingController nameController = TextEditingController();
+  final TextEditingController fatherNameController = TextEditingController();
+  final TextEditingController rollNoController = TextEditingController();
+  final TextEditingController semesterController = TextEditingController();
+  final TextEditingController programController = TextEditingController();
+  final TextEditingController facultyController = TextEditingController();
+
+  String message = '';
+
+  void createSemesterFeeVoucher() {
+    // Implement the logic to create a semester fee voucher here
+    // For demonstration purposes, I'm just showing a success message
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      message = 'Your semester fee voucher is created successfully!';
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text('Semester Fee Voucher'),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      body: SingleChildScrollView(
+        padding: EdgeInsets.all(20.0),
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+            TextField(
+              controller: nameController,
+              decoration: InputDecoration(labelText: 'Name'),
             ),
+            TextField(
+              controller: fatherNameController,
+              decoration: InputDecoration(labelText: 'Father\'s Name'),
+            ),
+            TextField(
+              controller: rollNoController,
+              decoration: InputDecoration(labelText: 'Roll No'),
+            ),
+            TextField(
+              controller: semesterController,
+              decoration: InputDecoration(labelText: 'Semester'),
+            ),
+            TextField(
+              controller: programController,
+              decoration: InputDecoration(labelText: 'Program'),
+            ),
+            TextField(
+              controller: facultyController,
+              decoration: InputDecoration(labelText: 'Faculty'),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                createSemesterFeeVoucher();
+              },
+              child: Text('Submit'),
+            ),
+            SizedBox(height: 20),
             Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+              message,
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+
+  @override
+  void dispose() {
+    nameController.dispose();
+    fatherNameController.dispose();
+    rollNoController.dispose();
+    semesterController.dispose();
+    programController.dispose();
+    facultyController.dispose();
+    super.dispose();
+  }
+}
+
+
+
+
+
+
+
+class RepeatFeePage extends StatefulWidget {
+  @override
+  _RepeatFeePageState createState() => _RepeatFeePageState();
+}
+
+class _RepeatFeePageState extends State<RepeatFeePage> {
+  final TextEditingController nameController = TextEditingController();
+  final TextEditingController fatherNameController = TextEditingController();
+  final TextEditingController rollNoController = TextEditingController();
+  final TextEditingController semesterController = TextEditingController();
+  final TextEditingController courseController = TextEditingController();
+  final TextEditingController facultyController = TextEditingController();
+
+  String message = '';
+
+  void createRepeatFeeVoucher() {
+    // Implement the logic to create a semester fee voucher here
+    // For demonstration purposes, I'm just showing a success message
+    setState(() {
+      message = 'Your Repeat Fee voucher is created successfully!';
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Repeat Fee Voucher'),
+      ),
+      body: SingleChildScrollView(
+        padding: EdgeInsets.all(20.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            TextField(
+              controller: nameController,
+              decoration: InputDecoration(labelText: 'Name'),
+            ),
+            TextField(
+              controller: fatherNameController,
+              decoration: InputDecoration(labelText: 'Father\'s Name'),
+            ),
+            TextField(
+              controller: rollNoController,
+              decoration: InputDecoration(labelText: 'Roll No'),
+            ),
+            TextField(
+              controller: semesterController,
+              decoration: InputDecoration(labelText: 'Semester'),
+            ),
+            TextField(
+              controller: courseController,
+              decoration: InputDecoration(labelText: 'Course'),
+            ),
+            TextField(
+              controller: facultyController,
+              decoration: InputDecoration(labelText: 'Faculty'),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                createRepeatFeeVoucher();
+              },
+              child: Text('Submit'),
+            ),
+            SizedBox(height: 20),
+            Text(
+              message,
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    nameController.dispose();
+    fatherNameController.dispose();
+    rollNoController.dispose();
+    semesterController.dispose();
+    courseController.dispose();
+    facultyController.dispose();
+    super.dispose();
+  }
+}
+
+
+
+
+
+
+
+
+class AdmissionFeePage extends StatefulWidget {
+  @override
+  _AdmissionFeePageState createState() => _AdmissionFeePageState();
+}
+
+class _AdmissionFeePageState extends State<AdmissionFeePage> {
+  final TextEditingController nameController = TextEditingController();
+  final TextEditingController fatherNameController = TextEditingController();
+  final TextEditingController semesterController = TextEditingController();
+  final TextEditingController facultyController = TextEditingController();
+
+  String message = '';
+
+  void createRepeatFeeVoucher() {
+    // Implement the logic to create a semester fee voucher here
+    // For demonstration purposes, I'm just showing a success message
+    setState(() {
+      message = 'Your Repeat Fee voucher is created successfully!';
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Admission Fee Voucher'),
+      ),
+      body: SingleChildScrollView(
+        padding: EdgeInsets.all(20.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            TextField(
+              controller: nameController,
+              decoration: InputDecoration(labelText: 'Name'),
+            ),
+            TextField(
+              controller: fatherNameController,
+              decoration: InputDecoration(labelText: 'Father\'s Name'),
+            ),
+            TextField(
+              controller: semesterController,
+              decoration: InputDecoration(labelText: 'Semester'),
+            ),
+            TextField(
+              controller: facultyController,
+              decoration: InputDecoration(labelText: 'Faculty'),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                createRepeatFeeVoucher();
+              },
+              child: Text('Submit'),
+            ),
+            SizedBox(height: 20),
+            Text(
+              message,
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    nameController.dispose();
+    fatherNameController.dispose();
+    semesterController.dispose();
+    facultyController.dispose();
+    super.dispose();
+  }
+}
+
+
+class FeeVoucherButton extends StatelessWidget {
+  final String label;
+  final VoidCallback onPressed;
+
+  const FeeVoucherButton({
+    required this.label,
+    required this.onPressed,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: onPressed,
+      child: Text(label),
+    );
+  }
+}
+
+class ContactPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Contact'),
+      ),
+      body: Center(
+        child: Text(
+          'Contact information goes here',
+          style: TextStyle(fontSize: 20),
+        ),
+      ),
+    );
+  }
+}
+
+class MenuPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Menu'),
+      ),
+      body: Center(
+        child: Text(
+          'Menu items go here',
+          style: TextStyle(fontSize: 20),
+        ),
+      ),
     );
   }
 }
